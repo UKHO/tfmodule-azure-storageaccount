84bd380f160f1e0a4768d200acae12ce66f237d8
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-07-01 14:59:34 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,30 @@
-variable "name" {
+variable "servicename" {
   type = string
-  default = "test"
 }
-
+variable "deploy_environment" {
+  type = string
+}
+variable "role" {
+  type = string
+}
+variable "resource_group_name" {
+  type = string
+}
+variable "resource_group_location" {
+  type = string
+}
+variable "account_kind" {
+  type = string
+}
+variable "account_tier" {
+  type = string
+}
+variable "account_replication_type" {
+  type = string
+}
+variable "access_tier" {
+  type = string
+}
+variable "enable_https_traffic_only" {
+  type = bool
+}
