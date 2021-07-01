fe77b38f5be169d892e0777029e947350f052376
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-07-01 14:59:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,24 @@ provider "azurerm" {
   alias = "src"
 }
 
-resource "azurerm_resource_group" "rg" {
-    provider = azurerm.src
-    name = "${var.name}-RG"  
-    location = "UK South"
+locals {
+  basename = lower("m${var.servicename}${var.role}${var.deploy_environment}")
 }
 
+resource "azurerm_storage_account" "logstashStorage" {
+  provider = azurerm.src
+  name                      = local.basename
+  resource_group_name       = var.resource_group_name
+  location                  = var.resource_group_location
+  account_kind              = var.account_kind
+  account_tier              = var.account_tier
+  account_replication_type  = var.account_replication_type
+  access_tier               = var.access_tier
+  enable_https_traffic_only = var.enable_https_traffic_only
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
