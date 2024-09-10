2f5156119c3cd081621c4b76dda4b28d296e22b3
pkent81 <130072235+pkent81@users.noreply.github.com>
2024-09-10 08:28:43 +0100

    Storage account with restricted public access (#1)
    * Add network rules
    
    * Updated module
    
    * Updated basename remoed role added sa at end
    
    * Added tls version and bypass for axure services
    
    * Added role and allowed_ips variables, ips now set in storage account rules
    
    * Updated network rule
    
    * Added features
    
    * removed provider block
    
    * Set provider on network rule
    
    * Added features
    
    * Removed provider block
    
    * Updated with condition on subnets and ip addresses
    
    * Updated readme file
    
    * Update README.md
    
    Added missing word from module description
    
    * Update README.md
    
    Updated variable description
    
    * Update variables.tf
    
    Updated variable description

--- main.tf
+++ main.tf
@@ -1,25 +1,48 @@
-provider "azurerm" {
-  alias = "src"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      configuration_aliases = [
+        azurerm.src
+      ]
+    }
+  }
 }
 
 locals {
-  basename = lower("m${var.servicename}${var.role}${var.deploy_environment}")
+  basename = lower("m${var.servicename}${var.role}${var.deploy_environment}sa")
 }
 
-resource "azurerm_storage_account" "logstashStorage" {
-  provider = azurerm.src
-  name                      = local.basename
-  resource_group_name       = var.resource_group_name
-  location                  = var.resource_group_location
-  account_kind              = var.account_kind
-  account_tier              = var.account_tier
-  account_replication_type  = var.account_replication_type
-  access_tier               = var.access_tier
-  enable_https_traffic_only = var.enable_https_traffic_only
+resource "azurerm_storage_account" "sa" {
+  provider                   = azurerm.src
+  name                       = local.basename
+  resource_group_name        = var.resource_group_name
+  location                   = var.resource_group_location
+  account_kind               = var.account_kind
+  account_tier               = var.account_tier
+  account_replication_type   = var.account_replication_type
+  access_tier                = var.access_tier
+  min_tls_version            = var.min_tls_version
+  https_traffic_only_enabled = var.https_traffic_only_enabled
 
   lifecycle {
     ignore_changes = [
       tags
     ]
+  }
+}
+
+resource "azurerm_storage_account_network_rules" "rules" {
+  provider                   = azurerm.src
+  storage_account_id         = azurerm_storage_account.sa.id
+  default_action             = "Deny"
+  virtual_network_subnet_ids = var.subnet_ids
+  bypass                     = ["AzureServices"]
+  ip_rules                   = var.allowed_ips
+  lifecycle {
+    precondition {
+      condition     = length(var.subnet_ids) + length(var.allowed_ips) != 0
+      error_message = "Using this module requires the subnet_ids or the allowed_ips array to contain at least one value."
+    }
   }
 }
